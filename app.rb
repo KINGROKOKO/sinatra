84228eb61458c4b38dc4fca1cba104968fe3e6c3
Thomas Ikimi <themerc01@gmail.com>
2017-09-12 15:24:25 +0100

    up to reading chrome dev tool

--- app.rb
+++ app.rb
@@ -17,8 +17,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @random_name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
