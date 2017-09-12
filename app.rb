0901cdbcaa3a13bf0a7af9facd1c62e9bd28a840
Thomas Ikimi <themerc01@gmail.com>
2017-09-12 12:16:39 +0100

    ERB keep views clean

--- app.rb
+++ app.rb
@@ -7,3 +7,12 @@ end
 get '/secret' do
   "I am here today"
 end
+
+get '/newpath' do
+   "new path here today"
+end
+
+get '/cat' do
+  @random_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
